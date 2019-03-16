aecc8007f9619cf6fd72117f02855f07271e5c0c
er86@njit.edu <godislove12>
2019-03-16 13:18:13 -0400

    added required files.

---
+++ layouts/default.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ebram
+ * Date: 3/16/19
+ * Time: 12:59 PM
+ */
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function()
+{
+    return View::make('pages.home');
 });
-
-Route::get('/about', function () {
-    return view('about');
+Route::get('about', function()
+{
+    return View::make('pages.about');
+});
+Route::get('projects', function()
+{
+    return View::make('pages.projects');
+});
+Route::get('contact', function()
+{
+    return View::make('pages.contact');
 });
