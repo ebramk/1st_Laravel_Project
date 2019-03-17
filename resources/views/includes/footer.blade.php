0935db863d289423a798e3b4277aa68f6197a259
er86@njit.edu <godislove12>
2019-03-17 18:39:34 -0400

    updated footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="row mt-4 mb-4"  class="font-weight-light">
-    &copy; 2019 Ebram Kerolos <a href="#"  class="font-weight-light"></a> &middo <a href="#"  class="font-weight-light"></a>
+    &copy; 2019 Ebram Kerolos</a>
 </div>
