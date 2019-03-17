9a652702c793cdca381bfaff2db0e1f891b2495a
er86@njit.edu <godislove12>
2019-03-17 18:03:17 -0400

    edit pages

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,2 +1,3 @@
-<div id="copyright text-right">© Copyright 2013 Scotchy Scotch Scotch</div>
-
+<div class="row mt-4 mb-4"  class="font-weight-light">
+    &copy; 2017-2018 Learana, Inc. &middot; <a href="#"  class="font-weight-light">Privacy</a> &middot; <a href="#"  class="font-weight-light">Terms</a>
+</div>
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,11 +1,11 @@
 <meta charset="utf-8">
-<meta name="description" content="">
-<meta name="author" content="Scotch">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="Learana Learning Analytics">
+<meta name="author" content="keith">
 
-<title>Super Cool Layouts</title>
+<title>Learana - Learning Analytics</title>
 
-<!-- load bootstrap from a cdn -->
-<linkrel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
-
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link href="/css/app.css" rel="stylesheet">
 
