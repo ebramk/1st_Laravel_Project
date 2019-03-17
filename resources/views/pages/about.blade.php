3fccdd7bc551c4bd6d578cdac97be327ae24de19
er86@njit.edu <godislove12>
2019-03-17 19:18:46 -0400

    added to about page 1

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.sidebar')
 @section('content')
-    A tiny bit about me, my name is Ebram and I’m a recent graduate with a bechler’s degree in Information Technology from NJIT. I’m currently pursing my MBA with a concentration in Management Information Systems.
+   A  tiny bit about me, my name is Ebram and I’m a recent graduate with a bechler’s degree in Information Technology from NJIT. I’m currently pursing my MBA with a concentration in Management Information Systems.
 @stop
