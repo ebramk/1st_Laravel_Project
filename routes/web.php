125094ab8a4181c70a386e7abbaeedffbe6b43f8
er86@njit.edu <godislove12>
2019-03-17 17:11:00 -0400

    fixed the views folder location issue. 500 error.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,45 @@
 <?php
 
-Route::get('/', function()
-{
-    return View::make('pages.home');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.index');
 });
-Route::get('about', function()
-{
-    return View::make('pages.about');
+Route::get('/meet-learana', function () {
+    return view('pages.why');
 });
-Route::get('projects', function()
-{
-    return View::make('pages.projects');
+
+Route::get('/connect', function () {
+    return view('pages.connect');
 });
-Route::get('contact', function()
-{
-    return View::make('pages.contact');
+
+Route::get('/why-learana', function () {
+    return view('pages.meet-learana');
+});
+
+Route::get('/discover-online-courses', function () {
+    return view('pages.discover');
+});
+Route::get('/login', function () {
+    return view('pages.login');
+});
+Route::get('/get-started', function () {
+    return view('pages.signup');
+});
+
+Route::get('/profile', function () {
+    return view('pages.profile');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
 });
