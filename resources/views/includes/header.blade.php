61d8a69645437957fb2f98da57ad822a59234d9b
er86@njit.edu <godislove12>
2019-03-17 18:23:46 -0400

    updated header file

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,36 +7,17 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item ">
-                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/discover-online-courses">Discover</a>
-                    </li>
-                    <li class="nav-item">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item ">
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
 
-                        <a class="nav-link" href="/why-learana">Why Learana?</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/meet-learana">Meet Learana</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/connect">Connect</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/login">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/get-started">Get Started</a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                    <button class="btn btn btn-success my-2 my-sm-0" type="submit">Find Courses</button>
-                </form>
-            </div>
+                <li class="nav-item">
+                    <a class="nav-link" href="/connect">Connect</a>
+                </li>
+
+            </ul>
+
         </div>
     </nav>
 </header>
