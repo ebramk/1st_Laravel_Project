274d3467d79945c045dd62fde20cfccad6f68ad6
er86@njit.edu <godislove12>
2019-03-27 13:49:03 -0400

    test

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,7 @@
   <h1>About me:</h1>
   <p>My name is Ebram and I’m a recent graduate with a bechler’s degree in Information Technology from NJIT. I’m currently pursing my MBA with a concentration in Management Information Systems. I am also working as a full time Systems Analyst at New Jersey Transit. My team specializes in developing and maintaining all of the customer facing technologies that NJ Transit has to offer. For example, my team maintains all of the station signage, automated announcements, departure vision and more.
 
-    I was born and raised in Egypt until the age of 11 and then came to the United States with my family.
+    I was born and I raised in Egypt until the age of 11 and then came to the United States with my family.
 
     1 Furthermore, some of my hobbies include programming, serving the community, and hanging out with my friends and family. Lastly, I’m looking forward to working with you all and learning more about programming.</p>
 
